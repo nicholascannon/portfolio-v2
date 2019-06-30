bf128544aef859384d647105fe7250479f01d231
Nicholas Cannon <nicholascannon1@gmail.com>
2019-06-30 17:42:50 +0800

    finished basic nav and added icons

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,6 +8,8 @@ import { Link } from 'react-router-dom';
 import { toggleNav, closeNav } from '../actions/navActions';
 
 import './Nav.css';
+import menu from '../icons/icons8-menu-filled.svg';
+import close from '../icons/iconfinder_icon-close-round_211651.svg';
 
 class Nav extends Component {
 	componentDidMount() {
@@ -29,27 +31,41 @@ class Nav extends Component {
 				ref={node => (this.refNode = node)}>
 				{this.props.nav.isOpen ? (
 					<div className="NavBox">
-						<button onClick={this.props.toggleNav}>X</button>
+						<button onClick={this.props.toggleNav}>
+							<img className="navIcon" src={close} />
+						</button>
 						<ul>
 							<li>
-								<Link to="/">Home</Link>
+								<Link to="/" onClick={this.props.closeNav}>
+									Home
+								</Link>
 							</li>
 							<li>
-								<Link to="/about">About</Link>
+								<Link to="/about" onClick={this.props.closeNav}>
+									About
+								</Link>
 							</li>
 							<li>
-								<Link to="/projects">Projects</Link>
+								<Link to="/projects" onClick={this.props.closeNav}>
+									Projects
+								</Link>
 							</li>
 							<li>
-								<Link to="/skills">Skills</Link>
+								<Link to="/skills" onClick={this.props.closeNav}>
+									Skills
+								</Link>
 							</li>
 							<li>
-								<Link to="/contact">Contact</Link>
+								<Link to="/contact" onClick={this.props.closeNav}>
+									Contact
+								</Link>
 							</li>
 						</ul>
 					</div>
 				) : (
-					<button onClick={this.props.toggleNav}>nav</button>
+					<button onClick={this.props.toggleNav}>
+						<img className="navIcon" src={menu} />
+					</button>
 				)}
 			</nav>
 		);
