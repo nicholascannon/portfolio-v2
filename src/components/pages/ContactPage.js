75ed09b1200a5994b2a58a05d8162b7fb815c426
Nicholas Cannon <nicholascannon1@gmail.com>
2019-07-08 17:20:32 +0800

    styled contact page

--- src/components/pages/ContactPage.js
+++ src/components/pages/ContactPage.js
@@ -3,13 +3,80 @@
  */
 import React from 'react';
 import Page from './Page';
+import { CSSTransition } from 'react-transition-group';
 
-const ContactPage = props => {
-	return (
-		<Page pageName="ContactPage">
-			<h1>Contact page</h1>
-		</Page>
-	);
-};
+import './ContactPage.css';
+
+class ContactPage extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: '',
+			msg: '',
+			email: '',
+			sent: false
+		};
+		this.onChange = e => {
+			this.setState({ [e.target.name]: e.target.value });
+		};
+	}
+
+	onSubmit = e => {
+		e.preventDefault();
+		this.setState({
+			email: '',
+			name: '',
+			msg: '',
+			sent: true
+		});
+	};
+
+	render() {
+		return (
+			<Page pageName="ContactPage">
+				{this.state.sent ? (
+					<h1>Your message has sent!</h1>
+				) : (
+					<CSSTransition>
+						<div className="contactBox">
+							<form method="POST">
+								<h1>Contact Me</h1>
+								<label for="name">Name</label>
+								<input
+									type="text"
+									name="name"
+									id="name"
+									value={this.state.name}
+									onChange={this.onChange}
+									placeholder="Name"
+								/>
+								<label for="name">Email</label>
+								<input
+									type="email"
+									name="email"
+									id="email"
+									value={this.state.email}
+									onChange={this.onChange}
+									placeholder="Email"
+								/>
+								<label for="msg">Message</label>
+								<textarea
+									name="msg"
+									id="msg"
+									rows="10"
+									value={this.state.msg}
+									onChange={this.onChange}
+									placeholder="Your message..."></textarea>
+								<button type="submit" onClick={this.onSubmit}>
+									SEND
+								</button>
+							</form>
+						</div>
+					</CSSTransition>
+				)}
+			</Page>
+		);
+	}
+}
 
 export default ContactPage;
