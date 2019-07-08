a6a99bed857e60056d9fa4dff4b99cff2c324671
Nicholas Cannon <nicholascannon1@gmail.com>
2019-07-08 17:37:31 +0800

    added transition to sent message for contact page

--- src/components/pages/ContactPage.js
+++ src/components/pages/ContactPage.js
@@ -3,7 +3,7 @@
  */
 import React from 'react';
 import Page from './Page';
-import { CSSTransition } from 'react-transition-group';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 import './ContactPage.css';
 
@@ -35,44 +35,44 @@ class ContactPage extends React.Component {
 		return (
 			<Page pageName="ContactPage">
 				{this.state.sent ? (
-					<h1>Your message has sent!</h1>
-				) : (
-					<CSSTransition>
-						<div className="contactBox">
-							<form method="POST">
-								<h1>Contact Me</h1>
-								<label for="name">Name</label>
-								<input
-									type="text"
-									name="name"
-									id="name"
-									value={this.state.name}
-									onChange={this.onChange}
-									placeholder="Name"
-								/>
-								<label for="name">Email</label>
-								<input
-									type="email"
-									name="email"
-									id="email"
-									value={this.state.email}
-									onChange={this.onChange}
-									placeholder="Email"
-								/>
-								<label for="msg">Message</label>
-								<textarea
-									name="msg"
-									id="msg"
-									rows="10"
-									value={this.state.msg}
-									onChange={this.onChange}
-									placeholder="Your message..."></textarea>
-								<button type="submit" onClick={this.onSubmit}>
-									SEND
-								</button>
-							</form>
-						</div>
+					<CSSTransition in={this.state.sent} timeout={200} classNames="contactFade" appear>
+						<h1>Your message has sent!</h1>
 					</CSSTransition>
+				) : (
+					<div className="contactBox">
+						<form method="POST">
+							<h1>Contact Me</h1>
+							<label for="name">Name</label>
+							<input
+								type="text"
+								name="name"
+								id="name"
+								value={this.state.name}
+								onChange={this.onChange}
+								placeholder="Name"
+							/>
+							<label for="name">Email</label>
+							<input
+								type="email"
+								name="email"
+								id="email"
+								value={this.state.email}
+								onChange={this.onChange}
+								placeholder="Email"
+							/>
+							<label for="msg">Message</label>
+							<textarea
+								name="msg"
+								id="msg"
+								rows="10"
+								value={this.state.msg}
+								onChange={this.onChange}
+								placeholder="Your message..."></textarea>
+							<button type="submit" onClick={this.onSubmit}>
+								SEND
+							</button>
+						</form>
+					</div>
 				)}
 			</Page>
 		);
