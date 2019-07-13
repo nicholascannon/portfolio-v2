abcc022ccdcc8ed6da526a5c062b02fa27a61e74
Nicholas Cannon <nicholascannon1@gmail.com>
2019-07-13 13:09:29 +0800

    Added more validation and comments to contact api

--- routes/contact.js
+++ routes/contact.js
@@ -2,14 +2,21 @@
  * Contact page API routes
  */
 const router = require('express').Router();
+const validate = require('validator');
+
 const emailService = require('../services/email');
 
+/**
+ * API endpoint to send contact message to admin email.
+ */
 router.post('/', async (req, res, next) => {
-	if (!req.body.email || !req.body.name || !req.body.msg) {
+	// Validate data
+	if (!req.body.email || !req.body.name || !req.body.msg)
 		return res.status(400).json({ msg: 'Please provide name, email and message' });
-	}
-
-	// TODO: validate and sanitise user data
+	if (!validate.isEmail(req.body.email))
+		return res.status(400).json({ msg: `${req.body.email} is not a valid email` });
+	if (req.body.name.trim() === '' || req.body.msg.trim() === '')
+		return res.status(400).json({ msg: 'Please provide your name and a message' });
 
 	try {
 		await emailService.sendContactMail(req.body.name, req.body.email, req.body.msg);
