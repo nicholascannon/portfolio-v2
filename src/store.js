a7536d93227463032fd923085c1630461efe2978
Nicholas Cannon <nicholascannon1@gmail.com>
2019-07-22 19:24:26 +0800

    Fixed redux dev tools bug when dev tools arent available

--- src/store.js
+++ src/store.js
@@ -14,6 +14,6 @@ export default createStore(
 	initState,
 	compose(
 		applyMiddleware(...middleware),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+		window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
 	)
 );
