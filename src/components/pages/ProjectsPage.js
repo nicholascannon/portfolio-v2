093cc24334ee7b3300848b71926be3588f619e9f
Nicholas Cannon <nicholascannon1@gmail.com>
2020-01-08 14:40:46 +0800

    project cleanup

--- src/components/pages/ProjectsPage.js
+++ src/components/pages/ProjectsPage.js
@@ -1,15 +1,44 @@
 /**
- * PorjectsPage.js
+ * ProjectsPage.js
  */
 import React from 'react';
 import Page from './Page';
+import { connect } from 'react-redux';
 
-const PorjectsPage = props => {
-	return (
-		<Page pageName="ProjectsPage">
-			<h1>Projects</h1>
-		</Page>
-	);
-};
+import './ProjectsPage.css';
 
-export default PorjectsPage;
+class ProjectsPage extends React.Component {
+	render() {
+		const { projects } = this.props;
+
+		return (
+			<Page pageName="ProjectsPage">
+				{projects ? (
+					projects.map(project => (
+						<div className="projectCard">
+							<h1>{project.name}</h1>
+							<hr />
+							<h3>Description:</h3>
+							<p>{project.body}</p>
+							<h3>Tech:</h3>
+							<ul>
+								{project.tech.map(tech => (
+									<li>{tech}</li>
+								))}
+							</ul>
+							<a href={project.url}>View Code</a>
+						</div>
+					))
+				) : (
+					<h1>No Projects</h1>
+				)}
+			</Page>
+		);
+	}
+}
+
+const mapStateToProps = state => ({
+	projects: state.projects.projects
+});
+
+export default connect(mapStateToProps, {})(ProjectsPage);
