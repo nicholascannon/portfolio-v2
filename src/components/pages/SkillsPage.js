d4e9e2b13a53602eca230634257e5a285899db50
Nicholas Cannon <nicholascannon1@gmail.com>
2020-03-15 21:22:03 +0800

    updated skills page

--- src/components/pages/SkillsPage.js
+++ src/components/pages/SkillsPage.js
@@ -23,61 +23,79 @@ const SkillsPage = props => {
 			<div className="rect"></div>
 			<Modal header="More skills" btnText="More skills">
 				<div>
-					<h3>Databases</h3>
-					<ul>
-						<li>MongoDB</li>
-						<li>MySQL</li>
-						<li>PostgresSQL</li>
-						<li>SQLite</li>
-					</ul>
-				</div>
-				<div>
 					<h3>Languages</h3>
 					<ul>
-						<li>JavaScript</li>
-						<li>Python</li>
-						<li>C / C++</li>
-						<li>HTML</li>
-						<li>CSS</li>
-						<li>Java</li>
+						<li>
+							Python <i>(advanced)</i>
+						</li>
+						<li>
+							JavaScript / Node.JS <i>(advanced)</i>
+						</li>
+						<li>
+							C / C++ <i>(proficient)</i>
+						</li>
+						<li>
+							HTML / CSS <i>(proficient)</i>
+						</li>
+						<li>
+							Java <i>(proficient)</i>
+						</li>
+						<li>
+							Go <i>(novice)</i>
+						</li>
+					</ul>
+				</div>
+				<div>
+					<h3>Databases</h3>
+					<ul>
+						<li>
+							PostgresSQL <i>(intermediate)</i>
+						</li>
+						<li>
+							MongoDB <i>(novice)</i>
+						</li>
+						<li>
+							MySQL <i>(intermediate)</i>
+						</li>
+						<li>
+							SQLite <i>(intermediate)</i>
+						</li>
+					</ul>
+				</div>
+				<div>
+					<h3>Frameworks / Libraries</h3>
+					<ul>
+						<li>
+							React / Redux <i>(proficient)</i>
+						</li>
+						<li>
+							ExpressJS <i>(proficient)</i>
+						</li>
+						<li>
+							Django <i>(proficient)</i>
+						</li>
+						<li>
+							Flask <i>(proficient)</i>
+						</li>
+						<li>
+							Numpy / Pandas <i>(proficient)</i>
+						</li>
+						<li>
+							Scikit-learn <i>(novice)</i>
+						</li>
 						<li>And more!</li>
-					</ul>
-				</div>
-				<div>
-					<h3>Frameworks</h3>
-					<ul>
-						<li>ReactJS</li>
-						<li>ExpressJS</li>
-						<li>JQuery</li>
-						<li>Bootsrtap</li>
-						<li>Django</li>
-						<li>Flask</li>
-					</ul>
-				</div>
-				<div>
-					<h3>DevOps / Cloud</h3>
-					<ul>
-						<li>AWS</li>
-						<li>Digital Ocean</li>
-						<li>Docker</li>
-						<li>NGINX</li>
-					</ul>
-				</div>
-				<div>
-					<h3>Design</h3>
-					<ul>
-						<li>PhotoShop</li>
-						<li>AdobeXD</li>
 					</ul>
 				</div>
 				<div>
 					<h3>Other</h3>
 					<ul>
-						<li>Git</li>
-						<li>GitHub</li>
-						<li>Bitbucket</li>
-						<li>TDD / BDD</li>
+						<li>Git version control</li>
+						<li>Test driven development</li>
 						<li>Agile development</li>
+						<li>AWS</li>
+						<li>Digital Ocean</li>
+						<li>Docker</li>
+						<li>NGINX</li>
 					</ul>
 				</div>
 			</Modal>
