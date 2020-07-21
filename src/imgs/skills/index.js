70920d452e376650082aba0d52648e319d0b5bc8
Nicholas Cannon <nicholascannon1@gmail.com>
2020-07-21 10:58:17 +0800

    added flask to skills

--- src/imgs/skills/index.js
+++ src/imgs/skills/index.js
@@ -9,13 +9,13 @@ import django from './django.svg';
 import docker from './docker.svg';
 import git from './git.svg';
 import sql from './sql.svg';
-import c from './c-plain.svg';
+import flask from './flask.svg';
 
 export default [
 	{ src: python, name: 'Python' },
 	{ src: django, name: 'Django' },
+	{ src: flask, name: 'Flask' },
 	{ src: sql, name: 'SQL' },
-	{ src: c, name: 'C' },
 	{ src: nodejs, name: 'Node.JS' },
 	{ src: react, name: 'React' },
 	{ src: docker, name: 'Docker' },
